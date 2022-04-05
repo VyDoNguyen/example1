e0ce2d7c51b8b5ebdf6b167c9d3771727296103e
Example1 <nguyend4@umbc.edu>
2022-04-04 22:11:02 -0400

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int x=2;
-    String a="Project example";
-        System.out.println(a);
-        System.out.println(x);
+
     }
 }
