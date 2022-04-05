a3065fa64cc662bae1f41ed565eb32e546ebb228
gator <feinnoah0@gmail.com>
2022-04-04 20:44:18 -0400

    prints stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	int x=2;
     String a="Project example";
+        System.out.println(a);
+        System.out.println(x);
     }
 }
