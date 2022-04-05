4ba6cf88eae99c8df917368b0d40707262811151
Example1 <nguyend4@umbc.edu>
2022-04-04 22:20:03 -0400

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+    // Registration Page
+            //1. Login Page if user already had an account_ VY NGUYEN
+        System.out.println("Username? ");
+        Scanner name = new Scanner(System.in);
+        String account = name.next();
+        System.out.println("Password? ");
+        String password = name.next();
+
+
+            //2. Sign up Page if user wants to create an account
+
+
+    // Account Details
+        //1. Account holder's information
+
+
+
+        //2. Actions: Withdraw, Deposit, Viewing Balance, Exit
+
+
+        // 3. Transaction details
+
+
+
 
     }
 }
