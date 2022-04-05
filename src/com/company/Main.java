de1ef04111e8ce0015022b35a9c5938dcdb8317a
Example1 <nguyend4@umbc.edu>
2022-04-04 22:08:52 -0400

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int x=2;
-    String a="Project example";
-        System.out.println(a);
-        System.out.println(x);
+        //Please Put your name on the part that you want to work on
+	//Registration
+        //1. Login Page if user's account has existed
+
+
+
+        //2. Sign up Page if user does not have an account
+
+
+
+     // Account Details
+        //1. Account holder's Name and DOB
+
+
+
+
+        //2. ID Number
+
+
+
+
+        //3. Transaction Details (date, amount, details)
+
+
+
+        //4. Withdraw, Deposit, or End action
+
     }
 }
