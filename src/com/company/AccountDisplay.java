d989ea7f211ffa0f389575f10b76421630b0314f
Nhat's Laptop <ff91178@umbc.edu>
2022-04-30 17:43:19 -0400

    2.0

--- src/com/company/AccountDisplay.java
+++ src/com/company/AccountDisplay.java
@@ -5,66 +5,126 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AccountDisplay extends AccountData {
-    private double balance;
+    private String email;
     private String address;
     private String name;
-    private String idNumber;
-    private double[] transactionCost;
-    private String[] transactionDate;
-    private String[] transactionPurpose;
+    private String accountNumber;
     private String dateOfBirth;
     private String phoneNum;
+    public String[] confirmedRequestedServices = new String[5];
+    public String[] assignedEmployees = new String[5];
+    private String randomIDNumber = getRandomNumber();
     static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/YYYY");
     static LocalDateTime now = LocalDateTime.now();
-    private String randomNumber=getRandomNumber();
-    Scanner input= new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
-    public void AccountData() {
-
-    }
-
-    public void AccountDetails(String name){
-        this.name=name;
-        idNumber=name.substring(0,1)+randomNumber;
-        setBalance();
+    public void AccountDetails(String name) {
+        this.name = name;
+        accountNumber = name.substring(0, 1) + randomIDNumber;
         System.out.println("Welcome " + name);
-        System.out.println("Today's Date "+ dtf.format(now));
-        System.out.println("Account Number " +idNumber);
-        System.out.println("Your account balance is $" + balance);
+        System.out.println("Today's Date " + dtf.format(now));
+        System.out.println("Account Number " + accountNumber);
+        displayRequests();
     }
-    public void askForOptions(){
-        int choice;
-        do {System.out.println("Press 1 for Transaction History, Press 2 to deposit funds, Press 3 to withdraw funds, Press 4 for more account details, Press 5 to refresh first page, Press 0 to Quit");
-            choice= input.nextInt();
-            switch (choice){
-                case (1):{ int i, j; for (i=0; i>transactionDate.length;i++){System.out.println(transactionDate[i]+ " " + transactionCost[i] + transactionPurpose[i]); break;}}
-                case (2):System.out.println("How much do you want to deposit "); balance=balance+ input.nextDouble(); break;
-                case (3):System.out.println("How much do you want to withdraw ");balance=balance- input.nextDouble(); break;
-                case (4): System.out.println("Date of Birth: " + dateOfBirth); System.out.println("Address: " + address); System.out.println("Cell Phone: "+phoneNum); break;
-                case (5): AccountDetails(name); break;
-                default: System.out.println("Not an option. Try again.");
+    public void displayRequests(){
+        int i;
+        System.out.println("Requested Services Assigned Employee");
+        for(i=0;i< confirmedRequestedServices.length;i++){
 
+            System.out.println(confirmedRequestedServices[i]+" "+assignedEmployees[i]);
 
         }
+    }
+    public void askForOptions() {
+        int choice, i;
+        int count = 0;
+        int size = 5;
+        String[] requestedServices = new String[size];
+        String[] assignedEmployee = new String[size];
+        System.out.println("Please select the services that you would like to request. Press 0 to confirm your requests, Press 9 to go to Main Page");
+        System.out.println("Press 1 for Internal Audit, Press 2 for Business Tax, Press 3 for Individual Tax, Press 4 for Consultation, Press 5 for Accounting");
+        do {
+            choice = input.nextInt();
+            switch (choice) {
+                case (1):
+                    requestedServices[count] = "Internal Audit";
+                    assignedEmployee[count] = "Noah Fein";
+                    count++;
+                    System.out.println("Internal Audit added!");
+                    break;
+                case (2):
+                    requestedServices[count] = "Business Tax";
+                    assignedEmployee[count] = "Vy Nguyen";
+                    count++;
+                    System.out.println("Business Tax added!");
+                    break;
+                case (3):
+                    requestedServices[count] = "Individual Tax";
+                    assignedEmployee[count] = "Nhat Han";
+                    count++;
+                    System.out.println("Individual Tax added!");
+                    break;
+                case (4):
+                    requestedServices[count] = "Consultation";
+                    assignedEmployee[count] = "Sophie Dong";
+                    count++;
+                    System.out.println("Consultation added!");
+                    break;
+                case (5):
+                    requestedServices[count] = "Accounting";
+                    assignedEmployee[count] = "Harsha Ambaty";
+                    count++;
+                    System.out.println("Accounting added!");
+                    break;
+                case (9):
+                    AccountDetails(name);
+                    displayRequests();
+                    break;
+                default:
+                    System.out.println("Not an option. Try again.");
+                case(0):break;
 
 
-    }while(choice!=0);}
-    public static String getRandomNumber(){
+            }
+
+        } while (choice != 0);
+        System.out.println("Press 1 to confirm your selections, Press 0 to restart");
+        for (i = 0; i < requestedServices.length; i++) {
+            System.out.println(requestedServices[i] + " " + assignedEmployee[i]);
+        }
+        choice = input.nextInt();
+        switch (choice) {
+            case (1):
+                int j;
+                for(j = 0;j < confirmedRequestedServices.length;j++) {
+                    confirmedRequestedServices[j]=requestedServices[j];
+                    assignedEmployees[j]=assignedEmployee[j];
+
+                }break;
+            case (0):
+                askForOptions();
+                break;
+            default:
+                System.out.println("Not an option. Try again."); break;
+        }
+
+    }
+
+    public static String getRandomNumber() {
         Random rnd = new Random();
         int number = rnd.nextInt(999999);
         return String.format("%06d", number);
     }
-    public void setBalance(){
-        balance=AccountData.balance;
-    }
 
 
     public static void main(String[] args) {
+        int choice=1;
         AccountDisplay accountDetails = new AccountDisplay();
-        accountDetails.AccountDetails("John");
-        accountDetails.askForOptions();
+        do {
+            accountDetails.AccountDetails("Katy Garcia");
+            accountDetails.askForOptions();
+        }while (choice!=0);
     }
-
     }
 
 
