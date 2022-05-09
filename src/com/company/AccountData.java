b2e1ee6cd50e683425cc39598794e6ef8b354ed7
Nhat's Laptop <ff91178@umbc.edu>
2022-05-09 18:09:43 -0400

    Added addressCheck and phoneNumCheck and accountDetails for an already existing account

--- src/com/company/AccountData.java
+++ src/com/company/AccountData.java
@@ -1,9 +1,33 @@
 package com.company;
 
-public class AccountData {
+public class AccountData extends AccountDisplay {
+    private String name="Alex Brown";
+    private String email="alexbrown@gmail.com";
+    private String address="1000 Hilltop Cir, Baltimore, MD";
+    private String phoneNum="410-455-1000";
+    private String dateOfBirth="October 13 2000";
+    private final String[] confirmedRequestedServices = new String[]{"Internal Audit"};
+    private final String[] assignedEmployees = new String[]{"Noah Fein"};
+
+
+    public AccountData(){
+        super();
+        AccountDetails(name, address, phoneNum, email, dateOfBirth);
+        displayPersonalInfo();
 
 
     }
+    public void displayRequests() {
+        int i;
+        System.out.println("Requested Services Assigned Employee");
+        for (i = 0; i < confirmedRequestedServices.length; i++) {
 
+            System.out.println(confirmedRequestedServices[i] + " " + assignedEmployees[i]);
+
+        }
+    }
+    public static void main(String[] args) {
+       new AccountData();
+}}
 
 
--- src/com/company/AccountDisplay.java
+++ src/com/company/AccountDisplay.java
@@ -4,37 +4,43 @@ import java.time.LocalDateTime;
 import java.util.Random;
 import java.util.Scanner;
 
-public class AccountDisplay extends AccountData {
+public class AccountDisplay  {
     private String email;
     private String address;
     private String name;
     private String accountNumber;
     private String dateOfBirth;
     private String phoneNum;
-    public String[] confirmedRequestedServices = new String[5];
-    public String[] assignedEmployees = new String[5];
+    private String[] confirmedRequestedServices = new String[5];
+    private String[] assignedEmployees = new String[5];
     private String randomIDNumber = getRandomNumber();
-    static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/YYYY");
-    static LocalDateTime now = LocalDateTime.now();
+    public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+    public static LocalDateTime now = LocalDateTime.now();
     Scanner input = new Scanner(System.in);
 
-    public void AccountDetails(String name) {
-        this.name = name;
+    public void AccountDetails(String name, String address, String phoneNum, String email, String dateOfBirth) {
+        setName(name);
+        setAddress(address);
+        setPhoneNum(phoneNum);
+        setEmail(email);
+        setDateOfBirth(dateOfBirth);
         accountNumber = name.substring(0, 1) + randomIDNumber;
         System.out.println("Welcome " + name);
         System.out.println("Today's Date " + dtf.format(now));
         System.out.println("Account Number " + accountNumber);
         displayRequests();
     }
-    public void displayRequests(){
+
+    public void displayRequests() {
         int i;
         System.out.println("Requested Services Assigned Employee");
-        for(i=0;i< confirmedRequestedServices.length;i++){
+        for (i = 0; i < confirmedRequestedServices.length; i++) {
 
-            System.out.println(confirmedRequestedServices[i]+" "+assignedEmployees[i]);
+            System.out.println(confirmedRequestedServices[i] + " " + assignedEmployees[i]);
 
         }
     }
+
     public void askForOptions() {
         int choice, i;
         int count = 0;
@@ -77,21 +83,29 @@ public class AccountDisplay extends AccountData {
                     System.out.println("Accounting added!");
                     break;
                 case (9):
-                    AccountDetails(name);
+                    AccountDetails(name, address, phoneNum, email, dateOfBirth);
                     displayRequests();
                     break;
                 default:
                     System.out.println("Not an option. Try again.");
-                case(0):break;
+                case (0):
+                    break;
 
 
             }
 
         } while (choice != 0);
-        System.out.println("Press 1 to confirm your selections, Press 0 to restart");
         for (i = 0; i < requestedServices.length; i++) {
             System.out.println(requestedServices[i] + " " + assignedEmployee[i]);
         }
+        Scanner input2=new Scanner(System.in);
+        System.out.println("Enter your address to confirm your identity");
+        String confirmAddress=input2.nextLine();
+        addressCheck(confirmAddress);
+        System.out.println("Enter your phone number");
+        String confirmPhoneNumber=input2.nextLine();
+        phoneNumCheck(confirmPhoneNumber);
+        System.out.println("Press 1 to confirm your selections, Press 0 to restart");
         choice = input.nextInt();
         switch (choice) {
             case (1):
@@ -115,13 +129,58 @@ public class AccountDisplay extends AccountData {
         int number = rnd.nextInt(999999);
         return String.format("%06d", number);
     }
-
+    public void setAddress(String address){
+        this.address=address;
+    }
+    public void setPhoneNum(String phoneNum){
+        this.phoneNum=phoneNum;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setDateOfBirth(String dob){
+        dateOfBirth=dob;
+    }
+    public void displayPersonalInfo(){
+        System.out.println(name);
+        System.out.println(dateOfBirth);
+        System.out.println(email);
+        System.out.println(address);
+        System.out.println(phoneNum);
+        System.out.println(dateOfBirth);
+    }
+    public void addressCheck(String givenAddress){
+        do {
+            if (givenAddress.equals(address)==true){
+                break;
+            }
+            else {
+                System.out.println("Incorrect Address. Try Again");
+                givenAddress= input.nextLine();
+            }
+        }while (givenAddress.equals(address)==false);
+    }
+    public void phoneNumCheck(String phoneNumber){
+        do{
+            if (phoneNumber.equals(phoneNum)==true){
+                break;
+            }
+            else{
+                System.out.println("Incorrect Phone Number. Try Again");
+                phoneNumber= input.nextLine();
+            }
+        }while (phoneNumber.equals(phoneNum)==false);
+    }
 
     public static void main(String[] args) {
         int choice=1;
         AccountDisplay accountDetails = new AccountDisplay();
         do {
-            accountDetails.AccountDetails("Katy Garcia");
+            accountDetails.AccountDetails("Katy Garcia", "1000 Circle Top Street", "410-310-2345", "FF91178", "October 13");
+            accountDetails.displayPersonalInfo();
             accountDetails.askForOptions();
         }while (choice!=0);
     }
