629696bdac30c8bcb711136a8fa9a423622ef119
gator <feinnoah0@gmail.com>
2022-05-12 22:59:09 -0400

    Added CustomerLoginPage to main.

---
+++ src/com/company/User.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class User {
+    public String userName;
+    public String password;
+    public String useremail;
+
+    User(String UserName, String Password, String UserEmail) {
+        userName = UserName;
+        password = Password;
+        useremail = UserEmail;
+    }
+}
