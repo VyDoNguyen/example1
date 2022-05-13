481e598c1212757d3144194a14451b883f8f805e
gator <feinnoah0@gmail.com>
2022-05-12 22:59:09 -0400

    Added CustomerLoginPage to main.

--- src/com/company/Combine.java
+++ src/com/company/Combine.java
@@ -1,10 +1,7 @@
 package com.company;
 
-public class Combine {
-    public static void main(String[] args) {
-            JoshBond client1 = new JoshBond();
-            client1.JoshBond();
-            demo data = new demo();
-            data.demo();
-    }
+class Combine {
+            void JoshBond () {
+            }
+            void demo() {}
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,1 +1,51 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        //user userData = new user("noahh", "3252", "ndsk@yahoo.com");
+        //user[] userData = new user[1];
+        //userData[0] = new user("bob1", "dhhjhcv", "fnvkng@yahoo.com");
+        ArrayList<User> list = new ArrayList<User>();
+        list.add(new User("bob123423", "122Dghghg", "toyyy@yahoo.com"));
+        list.add(new User("tom987654", "Wash23", "puff12@gmail.com"));
+        System.out.println("Welcome to 123 Accounting Firm");
+        Scanner thescn = new Scanner(System.in);
+        boolean validAnswer = false;
+        while(!validAnswer) {
+            System.out.println("Login: 'c' for customer, 'e' for employee");
+            String login = thescn.next();
+            if (login.equals("c") || login.equals("C")) {
+                System.out.println("call customer login");
+                boolean valid = false;
+                while(!valid) {
+                    System.out.println("Do you have an account: 'y' for yes, 'n' for no");
+                    String haveAccount = thescn.next();
+                    if (haveAccount.equals("y") || haveAccount.equals("Y")) {
+                        System.out.println("call login");
+                        loginPage.userLogin(list);
+                        valid = true;
+                    } else if (haveAccount.equals("n") || haveAccount.equals("N")) {
+                        System.out.println("call create account");
+                        User user = loginPage.createAccount(list);
+                        list.add(user);
+                        valid = true;
+                    } else {
+                        System.out.println("Invalid response!");
+                    }
+                }
+                validAnswer = true;
+            } else if (login.equals("e") || login.equals("E")) {
+                System.out.println("call employee login");
+                validAnswer = true;
+            } else {
+                System.out.println("Please enter a valid input!");
+            }
+        }
+
+    }
+}
 
