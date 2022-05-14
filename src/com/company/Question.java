100a6429639dd8b229c2b857b07767716c5e93d4
Example1 <nguyend4@umbc.edu>
2022-05-13 22:42:59 -0400

    Merge branch 'master' of https://github.com/VyDoNguyen/example1

---
+++ src/com/company/Question.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+public class Question {
+    public void Question() {
+        int i;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Are you an employee or a customer. Press 1 for employee or Press 2 for customer");
+            i = input.nextInt();
+            switch (i) {
+                case (1):
+                    CustomerLoginPage login1 = new CustomerLoginPage();
+                case (2):
+                    EmployeeLoginPage login2 = new EmployeeLoginPage();
+                default:
+                    System.out.println("Not an option. Try again.");
+            }
+        } while(i!=1||i!=2);
+    }
+}
