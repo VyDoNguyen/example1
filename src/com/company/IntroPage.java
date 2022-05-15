68c29eb6ee04de1ad52da722371a186ed79a391c
dongsophie <103700451+dongsophie@users.noreply.github.com>
2022-05-15 03:42:53 -0400

    Update IntroPage.java

--- src/com/company/IntroPage.java
+++ src/com/company/IntroPage.java
@@ -1,47 +1,64 @@
-package com.company;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class IntroPage {
-    public static void IntroPage(ArrayList<String> list) {
-        System.out.println("Welcome to 123 Accounting Firm");
-        Scanner thescn = new Scanner(System.in);
-        AccountData accountData = new AccountData();
-        boolean validAnswer = false;
-        while (!validAnswer) {
-            System.out.println("Login: 'c' for customer, 'e' for employee");
-            String login = thescn.next();
-            if (login.equals("c") || login.equals("C")) {
-                boolean valid = false;
-                while (!valid) {
-                    System.out.println("Do you have an account: 'y' for yes, 'n' for no");
-                    String haveAccount = thescn.next();
-                    if (haveAccount.equals("y") || haveAccount.equals("Y")) {
-                        CustomerLoginPage.userLogin();
-                        accountData.getAccountData();
-                        valid = true;
-                    } else if (haveAccount.equals("n") || haveAccount.equals("N")) {
-                        CustomerLoginPage.getPassword(list);
-                        CustomerLoginPage.getUsername(list);
-                        CustomerLoginPage.getEmail();
-                        CustomerLoginPage.getPhoneNumber();
-                        CustomerLoginPage.getDob();
-                        CustomerLoginPage.getUserAddress();
-                        valid = true;
-                    } else {
-                        System.out.println("Invalid response!");
+    public class IntroPage {
+        public static void IntroPage(ArrayList<String> list) {
+            System.out.println("Welcome to 123 Accounting Firm");
+            Scanner thescn = new Scanner(System.in);
+            AccountData accountData = new AccountData();
+            boolean validAnswer = false;
+            while (!validAnswer) {
+                System.out.println("Login: 'c' for customer, 'e' for employee");
+                String login = thescn.next();
+                if (login.equals("c") || login.equals("C")) {
+                    boolean valid = false;
+                    while (!valid) {
+                        System.out.println("Do you have an account: 'y' for yes, 'n' for no");
+                        String haveAccount = thescn.next();
+                        if (haveAccount.equals("y") || haveAccount.equals("Y")) {
+                            CustomerLoginPage.userLogin();
+                            accountData.getAccountData();
+                            valid = true;
+                        } else if (haveAccount.equals("n") || haveAccount.equals("N")) {
+                            CustomerLoginPage.getPassword(list);
+                            CustomerLoginPage.getUsername(list);
+                            CustomerLoginPage.getEmail();
+                            CustomerLoginPage.getPhoneNumber();
+                            CustomerLoginPage.getTheUserDob();
+                            CustomerLoginPage.getUserAddress();
+                            valid = true;
+                        } else {
+                            System.out.println("Invalid response!");
+                        }
                     }
+                    validAnswer = true;
+                } else if (login.equals("e") || login.equals("E")) {
+                    boolean valid = false;
+                    while (!valid) {
+                        System.out.println("Do you have an account: 'y' for yes, 'n' for no");
+                        String haveAccount = thescn.next();
+                        if (haveAccount.equals("y") || haveAccount.equals("Y")) {
+                            EmployeeProfile.employeeLogin();
+                            accountData.getAccountData();
+                            valid = true;
+                        } else if (haveAccount.equals("n") || haveAccount.equals("N")) {
+                            EmployeeProfile.getEmployeePassword(list);
+                            EmployeeProfile.getEmployeeName(list);
+                            EmployeeProfile.getEmployeeEmail();
+                            EmployeeProfile.getEmployeeNumber();
+                            EmployeeProfile.getEmployeeDob();
+                            EmployeeProfile.getEmployeeAddress();
+                            valid = true;
+                        } else {
+                            System.out.println("Invalid response!");
+                        }
+                    }
+                    validAnswer = true;
+                } else {
+                    System.out.println("Please enter a valid input!");
                 }
-                validAnswer = true;
-            } else if (login.equals("e") || login.equals("E")) {
-                EmployeeLoginPage.EmployeeLoginPage();
-                validAnswer = true;
-            } else {
-                System.out.println("Please enter a valid input!");
             }
         }
+
+
     }
-
-
-}
